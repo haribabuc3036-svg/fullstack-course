bb0361fd28fe8b1e4c994161e5ccae41aad341ca
haribabu.c3036@gmail.com <haribabu.c3036@gmail.com>
2026-04-22 21:42:03 +0530

    completed till css

---
+++ js/script.js
@@ -0,0 +1,97 @@
+/* console.log(4)
+console.log(2+2) */
+
+// console.log(3-2)
+
+// console.log("hari")
+
+// 2+2+2
+// console.log(2+2+2-3)
+
+// Variables  = data containers
+
+
+// let fullName="babu"
+// you can reassign the values when declared using the let
+// const fruit="orange"
+// you cannot reassing the value 
+// fullName="hari babu"
+// fullName="c hari babu"
+
+
+// let orangePrice=10;
+
+
+// let applePrice=20;
+// orangePrice=15
+// applePrice=30;
+
+// let pi=3.14
+// pi=3.15
+
+// let name="vishnu"
+
+// let orangePrice2=10
+
+
+// console.log(fullName)
+// console.log(fullName)
+// console.log(fullName)
+// console.log(fullName)
+// console.log(fullName)
+// console.log(fruit)
+// console.log(orangePrice+applePrice,"overall price")
+// console.log(pi)
+// console.log(name)
+
+// document.getElementById("test").textContent=name
+
+
+// let fullName="hari babu"
+// rules of naming a variable (conventions)
+// A-Z,a-z,0-9,$,_
+
+// let firstName="vishnu"
+
+//  should not contain white space in between
+
+// let first name="vishnu"
+
+// names cannot be keywords
+
+// let function="hari"
+
+
+// javascript is case sensitive First is not equals to first
+// let first="vishnu"
+// let First="harsha"
+
+// console.log(first,First)
+
+// names should not start with numbers
+
+// let 1stname="hari"
+// let 1stprize=100;
+// let 2ndprize=50
+
+// let prize1=100;
+
+// console.log(prize1)
+
+// try to use meaningful names
+
+// let job1="developer"
+// let job2="teacher"
+
+// let firstJob="devloper"
+// let secondJob="teacher"
+
+// camelcase in js - everydeveloper will use camelcase in javascript
+
+
+let hariFirstJob="developer"
+let fullName=""
+let harifirstjob="developer"
+let hari_first_job="developer"
+
+// hari_first_job="deveoper"
